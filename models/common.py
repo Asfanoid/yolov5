a63a61fa0d91a1660ebb6270b662c06fd0e05010
Asfand <Asfandzariat@gmail.com>
2021-10-01 03:21:15 +0200

    Update common.py
    change Mish to Swish

--- models/common.py
+++ models/common.py
@@ -132,7 +132,7 @@ class BottleneckCSP2(nn.Module):
         self.cv3 = nn.Conv2d(c_, c_, 1, 1, bias=False)
         self.cv4 = Conv(2 * c_, c2, 1, 1)
         self.bn = nn.BatchNorm2d(2 * c_)  # applied to cat(cv2, cv3)
-        self.act = nn.Mish(inplace=False)
+        self.act = nn.SiLU(inplace=False)
         self.m = nn.Sequential(*[Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)])
 
     def forward(self, x):
